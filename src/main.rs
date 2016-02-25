6fd132e41015165cda7f1e16bcb1b0e67d040ecd
Ivan Bodrov <ibodrov@jet.msk.su>
2016-02-25 23:52:21 +0300

    proper ortho

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,148 @@
 #[macro_use]
 extern crate glium;
+extern crate rand;
 extern crate image;
+extern crate time;
 
 use glium::{DisplayBuild, Surface};
 use glium::index::PrimitiveType;
 use glium::glutin;
 
-const MAP_WIDTH: u32 = 32;
-const MAP_HEIGHT: u32 = 32;
-const TILE_WIDTH: u32 = 10;
-const TILE_HEIGHT: u32 = 12;
-const TILES_PER_ROW: u32 = 16;
-
 fn main() {
+    const SCREEN_W: u32 = 1024;
+    const SCREEN_H: u32 = 768;
+    const MAP_WIDTH: usize = 32;
+    const MAP_HEIGHT: usize = 32;
+    const TILE_W: f32 = 12.0;
+    const TILE_H: f32 = 12.0;
+    const SPRITES_COUNT: usize = MAP_WIDTH * MAP_HEIGHT;
+
     let display = glutin::WindowBuilder::new()
-        .with_dimensions(800, 600)
+        .with_dimensions(SCREEN_W, SCREEN_H)
         .build_glium()
         .unwrap();
-
-    let tiles = {
-        let i = image::open(std::path::Path::new("tiles.png")).unwrap().to_rgba();
-        let d = i.dimensions();
-        let i = glium::texture::RawImage2d::from_raw_rgba_reversed(i.into_raw(), d);
-        glium::texture::Texture2d::new(&display, i).unwrap()
-    };
 
     let (vertex_buffer, index_buffer) = {
         #[derive(Copy, Clone)]
         struct Vertex {
             i_position: [f32; 2],
-            i_tex_coords: [f32; 2],
+            i_color: f32,
         }
 
-        impl Vertex {
-            fn new(pos: [f32; 2], tex: [f32; 2]) -> Vertex {
-                Vertex { i_position: pos, i_tex_coords: tex }
-            }
+        implement_vertex!(Vertex, i_position, i_color);
+
+        let mut vb: glium::VertexBuffer<Vertex> = glium::VertexBuffer::empty_dynamic(&display, SPRITES_COUNT * 4).unwrap();
+        let mut ib_data = Vec::with_capacity(SPRITES_COUNT * 6);
+
+        for (num, sprite) in vb.map().chunks_mut(4).enumerate() {
+            let color = if num == 0 { 0.5 } else if num % 2 == 0 { 1.0 } else { 0.0 };
+            let i = (num % MAP_WIDTH) as f32;
+            let j = (num / MAP_WIDTH) as f32;
+
+            let x = i * TILE_W + TILE_W;
+            let y = j * TILE_H + TILE_H;
+
+            let position: (f32, f32) = (x, y);
+
+            sprite[0].i_position[0] = position.0 - TILE_W;
+            sprite[0].i_position[1] = position.1 + TILE_H;
+            sprite[0].i_color = color;
+            sprite[1].i_position[0] = position.0 + TILE_W;
+            sprite[1].i_position[1] = position.1 + TILE_H;
+            sprite[1].i_color = color;
+            sprite[2].i_position[0] = position.0 - TILE_W;
+            sprite[2].i_position[1] = position.1 - TILE_H;
+            sprite[2].i_color = color;
+            sprite[3].i_position[0] = position.0 + TILE_W;
+            sprite[3].i_position[1] = position.1 - TILE_H;
+            sprite[3].i_color = color;
+
+            let num = num as u16;
+            ib_data.push(num * 4);
+            ib_data.push(num * 4 + 1);
+            ib_data.push(num * 4 + 2);
+            ib_data.push(num * 4 + 1);
+            ib_data.push(num * 4 + 3);
+            ib_data.push(num * 4 + 2);
         }
 
-        implement_vertex!(Vertex, i_position, i_tex_coords);
-
-        let vb_cnt = (MAP_WIDTH * MAP_HEIGHT * 4) as usize;
-        let mut vb_data = Vec::<Vertex>::with_capacity(vb_cnt);
-
-        let ib_cnt = (MAP_WIDTH * MAP_HEIGHT * 6) as usize;
-        let mut ib_data = Vec::<u16>::with_capacity(ib_cnt);
-
-        let tile_w = (TILE_WIDTH as f32) / 400.0;
-        let tile_h = (TILE_HEIGHT as f32) / 300.0;
-
-        for i in 0..MAP_WIDTH {
-            for j in 0..MAP_HEIGHT {
-                let tex_id = 1;
-
-                let tu = (tex_id % TILES_PER_ROW) as f32;
-                let tv = (tex_id / TILES_PER_ROW) as f32;
-
-                let x = (i * TILE_WIDTH) as f32;
-                let y = (j * TILE_HEIGHT) as f32;
-
-                let (x1, y1, u1, v1) = (x - tile_w, y + tile_h, tu * tile_w, tv * tile_h);
-                let (x2, y2, u2, v2) = (x + tile_w, y + tile_w, (tu + 1.0) * tile_w, tv * tile_h);
-                let (x3, y3, u3, v3) = (x - tile_w, y - tile_w, (tu + 1.0) * tile_w, (tv + 1.0) * tile_h);
-                let (x4, y4, u4, v4) = (x + tile_w, y - tile_h, tu * tile_w, (tv + 1.0) * tile_h);
-
-                vb_data.push(Vertex::new([x1, y1], [u1, v1]));
-                vb_data.push(Vertex::new([x2, y2], [u2, v2]));
-                vb_data.push(Vertex::new([x3, y3], [u3, v3]));
-                vb_data.push(Vertex::new([x4, y4], [u4, v4]));
-
-                let n = (i + j * MAP_WIDTH) as u16;
-                ib_data.push(n * 4);
-                ib_data.push(n * 4 + 1);
-                ib_data.push(n * 4 + 2);
-                ib_data.push(n * 4 + 1);
-                ib_data.push(n * 4 + 3);
-                ib_data.push(n * 4 + 2);
-            }
-        }
-
-        let vb = glium::VertexBuffer::<Vertex>::new(&display, &vb_data).unwrap();
-        let ib = glium::IndexBuffer::new(&display, PrimitiveType::TrianglesList, &ib_data).unwrap();
-
-        (vb, ib)
+        (vb, glium::index::IndexBuffer::new(&display, PrimitiveType::TrianglesList, &ib_data).unwrap())
     };
 
-    let vertex_shader_src = r#"
-        #version 140
+    // we determine the texture coordinates depending on the ID the of vertex
+    let program = program!(&display,
+        140 => {
+            vertex: "
+                #version 140
 
-        in vec2 i_position;
-        in vec2 i_tex_coords;
+                uniform mat4 matrix;
 
-        out vec2 v_tex_coords;
+                in vec2 i_position;
+                in float i_color;
 
-        void main() {
-            gl_Position = vec4(i_position, 0.0, 1.0);
-            v_tex_coords = i_tex_coords;
-        }
-    "#;
+                out float v_color;
 
-    let fragment_shader_src = r#"
-        #version 140
+                void main() {
+                    gl_Position = matrix * vec4(i_position, 0.0, 1.0);
+                    v_color = i_color;
+                }
+            ",
 
-        uniform sampler2D tex;
+            fragment: "
+                #version 140
 
-        in vec2 v_tex_coords;
+                in float v_color;
 
-        out vec4 f_color;
+                out vec4 f_color;
 
-        void main() {
-            f_color = texture2D(tex, v_tex_coords);
-        }
-    "#;
+                void main() {
+                    f_color = vec4(v_color, 0.0, 0.0, 1.0);
+                }
+            "
+        },
+    ).unwrap();
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-    let uniforms = uniform! {
-        texture: &tiles,
-    };
+    let mut cnt = 0;
+    let mut t0 = time::precise_time_s();
 
     loop {
+        let ib_slice = index_buffer.slice(0 .. SPRITES_COUNT * 6).unwrap();
+
+        let right = SCREEN_W as f32;
+        let bottom = SCREEN_H as f32;
+        let left = 0.0;
+        let top = 0.0;
+        let far = 1.0;
+        let near = -1.0;
+
+        let uniforms = uniform! {
+            matrix: [
+                [2.0 / (right - left),             0.0,                              0.0,                          0.0],
+                [0.0,                              2.0 / (top - bottom),             0.0,                          0.0],
+                [0.0,                              0.0,                              -2.0 / (far - near),          0.0],
+                [-(right + left) / (right - left), -(top + bottom) / (top - bottom), -(far + near) / (far - near), 1.0f32],
+            ],
+        };
+
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &Default::default()).unwrap();
+        target.clear_color(0.0, 0.0, 0.0, 0.0);
+        target.draw(&vertex_buffer, &ib_slice, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
 
-        for e in display.poll_events() {
-            match e {
+        for event in display.poll_events() {
+            match event {
                 glutin::Event::Closed => return,
                 _ => ()
             }
         }
-    }
+
+        cnt += 1;
+        let t1 = time::precise_time_s();
+        let dt = t1 - t0;
+        if dt >= 1.0 {
+            println!("FPS: {}", cnt);
+            cnt = 0;
+            t0 = t1;
+        }
+    };
 }
